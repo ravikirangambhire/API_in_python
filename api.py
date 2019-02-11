ae195ffb2c0cd59c941f5054f8083f336ea49925
ravikirangambhire <ravikirangambhire599@gmail.com>
2019-02-12 00:10:51 +0530

    added database and code to fetch from database

--- api.py
+++ api.py
@@ -1,7 +1,6 @@
 import flask
 from flask import request, jsonify
 
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -30,10 +29,32 @@ def home():
     return '''<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
+
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
     return jsonify(books)
 
-app.run()
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
 
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+app.run()
